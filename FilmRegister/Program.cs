328fce571f113378bf1a4c37e75e07e6c85c167d
OliverDahlqvist <oliverdahlqvist97@gmail.com>
2020-02-03 10:56:42 +0100

    Test commit

---
+++ FilmRegister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilmRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
